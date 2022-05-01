b22d576393f9cb11a7995afb9d0b3320c6ac6185
Robert <robert.janaszek@gmail.com>
2022-05-01 21:56:00 +0200

    Remove 'resetServerContext' since it doesn't work anyway

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,25 +1,9 @@
 import * as React from 'react';
-import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
-import { ServerStyles, createStylesServer } from '@mantine/next';
-import { resetServerContext } from 'react-beautiful-dnd';
-
-const stylesServer = createStylesServer();
+import Document, {  Head, Html, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from '@mantine/next';
 
 export default class _Document extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    resetServerContext();
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <ServerStyles html={initialProps.html} server={stylesServer} />
-        </>
-      ) as any, // TODO: fix typing
-    };
-  }
+  static getInitialProps = createGetInitialProps();
   
   render() {
     return (
