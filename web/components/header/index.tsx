ba95c5b6893ebcda5f8613f231432b4e265681ae
Robert <robert.janaszek@gmail.com>
2022-05-02 21:37:20 +0200

    Reworking header

--- web/components/header/index.tsx
+++ web/components/header/index.tsx
@@ -1,12 +1,22 @@
-import { useMantineColorScheme, ActionIcon, Header } from '@mantine/core';
-import { Sun, MoonStars } from 'tabler-icons-react';
+import { useMantineColorScheme, ActionIcon, Header, Group, Text, Input } from '@mantine/core';
+import { Sun, MoonStars, Search } from 'tabler-icons-react';
 
 export const AppHeader = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
-  return <Header height={60} p="xs">
-    <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
-      {colorScheme === 'dark' ? <Sun size={16} /> : <MoonStars size={16} />}
-    </ActionIcon>
+  return <Header height={50} p="xs">
+    <Group position="apart">
+      <Text>Jira Clone</Text>
+      <Group>
+        <Input
+          icon={<Search />}
+          placeholder="Search issues"
+          size="xs"
+        />
+        <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
+          {colorScheme === 'dark' ? <Sun size={16} /> : <MoonStars size={16} />}
+        </ActionIcon>
+      </Group>
+    </Group>
   </Header>;
 }
