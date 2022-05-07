11c3f1a11d9fb215c5271e8e07aed21d7242f5d0
Robert <robert.janaszek@gmail.com>
2022-05-07 14:31:12 +0200

    Fix styling for page

--- web/pages/project/[projectId]/settings.tsx
+++ web/pages/project/[projectId]/settings.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, LoadingOverlay, Select, TextInput } from '@mantine/core';
+import { Button, Container, Group, LoadingOverlay, Select, Text, TextInput } from '@mantine/core';
 import { useRouter } from 'next/router';
 import { Breadcrumb } from '../../../components/breadcrumb';
 import { useDiscardProjectUpdates, useIsProjectModified, useProject, useProjectCategory, useProjectDescription, useProjectName, useUpdateProject } from '../../../module/project/service';
@@ -21,33 +21,42 @@ const Board = () => {
     <Breadcrumb projectId={projectId} page="Settings" />
     <div style={{ position: 'relative' }}>
       <LoadingOverlay visible={isFetching} />
-      <TextInput
-        placeholder="Project name"
-        label="Type in project name"
-        required
-        value={projectName}
-        onChange={(event) => updateProjectName(event.currentTarget.value)}
-      />
-      <RichTextEditor value={description} onChange={updateDescription} />
-      <Select
-        label="Project category"
-        placeholder="Choose project category"
-        data={[
-          { value: 'software', label: 'Software' },
-          { value: 'marketing', label: 'Marketing' },
-          { value: 'business', label: 'Business' },
-        ]}
-        value={projectCategory}
-        onChange={(value) => updateProjectCategory(value)}
-      />
-      <Group>
-        <Button onClick={() => update.mutate()} disabled={!isProjectModified}>
-          Save changes
-        </Button>
-        <Button onClick={() => discard()}>
-          Discard
-        </Button>
-      </Group>
+      <Container py="sm">
+        <TextInput
+          placeholder="Project name"
+          label="Type in project name"
+          required
+          value={projectName}
+          onChange={(event) => updateProjectName(event.currentTarget.value)}
+        />
+      </Container>
+      <Container py="sm">
+        <Text>Description</Text>
+        <RichTextEditor value={description} onChange={updateDescription} style={{ minHeight: 200 }} />
+      </Container>
+      <Container py="sm">
+        <Select
+          label="Project category"
+          placeholder="Choose project category"
+          data={[
+            { value: 'software', label: 'Software' },
+            { value: 'marketing', label: 'Marketing' },
+            { value: 'business', label: 'Business' },
+          ]}
+          value={projectCategory}
+          onChange={(value) => updateProjectCategory(value)}
+        />
+      </Container>
+      <Container py="sm">
+        <Group>
+          <Button onClick={() => update.mutate()} disabled={!isProjectModified}>
+            Save changes
+          </Button>
+          <Button onClick={() => discard()}>
+            Discard
+          </Button>
+        </Group>
+      </Container>
     </div>
   </>;
 }
