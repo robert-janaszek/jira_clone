f2090cebaa67ba93251f7831c3eef6572bef29e9
Robert <robert.janaszek@gmail.com>
2022-05-07 23:42:21 +0200

    Render project description using text on server

--- web/components/rte/index.tsx
+++ web/components/rte/index.tsx
@@ -1,9 +1,19 @@
-import { Center, Loader } from '@mantine/core';
-import dynamic from 'next/dynamic';
+import { Loader, Text } from '@mantine/core';
+import { RichTextEditorProps } from '@mantine/rte';
+import { useEffect, useState } from 'react';
+import RichTextEditor from './rte';
 
-export default dynamic(() => import('@mantine/rte'), {
-  ssr: false,
-  loading: () => <Center>
-    <Loader style={{ minHeight: 200 }}/>
-  </Center>,
-});
+export default (props: RichTextEditorProps) => {
+  const [canRender, setCanRender] = useState(false);
+  useEffect(() => setCanRender(true), []);
+
+  if (!canRender) {
+    const { value } = props
+    if (typeof value === 'string') {
+      return <Text style={props.style}>{value}</Text>
+    }
+    return <Loader style={{ minHeight: 200 }}/>
+  }
+
+  return <RichTextEditor {...props} />;
+}
---
+++ web/components/rte/rte.tsx
@@ -0,0 +1,9 @@
+import { Center, Loader } from '@mantine/core';
+import dynamic from 'next/dynamic';
+
+export default dynamic(() => import('@mantine/rte'), {
+  ssr: false,
+  loading: () => <Center>
+    <Loader style={{ minHeight: 200 }}/>
+  </Center>,
+});
